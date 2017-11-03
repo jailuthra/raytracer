965d23a69c40d387f2ea2d347a7884af4a8dd322
abrolmrinal <mrinal15062@iiitd.ac.in>
2017-11-03 10:09:31 +0530

    tab to spaces

--- src/sphere.cpp
+++ src/sphere.cpp
@@ -4,45 +4,45 @@
 
 bool Sphere::hit(const Ray &ray, double t0, double t1, HitRec &hr) {
 
-	glm::vec3 centerVec = ray.origin - pos;
-	double A = 1.0; // glm::dot(ray.dir, ray.dir)
-	double B = 2 * glm::dot(ray.dir, centerVec);
-	double C = glm::dot(centerVec, centerVec) - radius*radius;
-	double Discriminant = B*B - 4.0*A*C;
+    glm::vec3 centerVec = ray.origin - pos;
+    double A = 1.0; // glm::dot(ray.dir, ray.dir)
+    double B = 2 * glm::dot(ray.dir, centerVec);
+    double C = glm::dot(centerVec, centerVec) - radius*radius;
+    double Discriminant = B*B - 4.0*A*C;
 
-	bool stat = false;
+    bool stat = false;
 
-	if(Discriminant == 0.0){
-		double t;
-		t = -B / (2.0 * A);
+    if(Discriminant == 0.0){
+        double t;
+        t = -B / (2.0 * A);
 
-		if(t < t1 && t > t0){
-			hr.hit = true;
-			hr.t = t;
-			hr.s = this;
-		}
-		stat = true;
-	}
+        if(t < t1 && t > t0){
+            hr.hit = true;
+            hr.t = t;
+            hr.s = this;
+        }
+        stat = true;
+    }
 
-	else if(Discriminant > 0.0){
-		double D = sqrt(Discriminant);
-		double r0 = (-B + D) / (2.0 * A);
-		double r1 = (-B - D) / (2.0 * A);
+    else if(Discriminant > 0.0){
+        double D = sqrt(Discriminant);
+        double r0 = (-B + D) / (2.0 * A);
+        double r1 = (-B - D) / (2.0 * A);
 
-		double t = r0 < r1 ? r0 : r1;
-		
-		if(t < t1 && t > t0){
-			hr.hit = true;
-			hr.t = t;
-			hr.s = this;
-			stat = true;
-		}
-	}
+        double t = r0 < r1 ? r0 : r1;
+        
+        if(t < t1 && t > t0){
+            hr.hit = true;
+            hr.t = t;
+            hr.s = this;
+            stat = true;
+        }
+    }
 
-	return stat;
+    return stat;
 }
 
 glm::vec3 Sphere::getNormal(glm::vec3 hitpoint) const {
-	glm::vec3 normal = glm::normalize(hitpoint - pos);
-	return normal;
+    glm::vec3 normal = glm::normalize(hitpoint - pos);
+    return normal;
 }
--- src/triangle.h
+++ src/triangle.h
@@ -1,18 +1,18 @@
 #ifndef __TRIANGLE_H
 #define __TRIANGLE_H
-	
+    
 #include "surface.h"
 #include <glm/vec3.hpp>
 
 class Triangle : public Surface {
 public: 
-	glm::vec3 v1, v2, v3; 
+    glm::vec3 v1, v2, v3; 
 
-	Triangle(glm::vec3 _v1, glm::vec3 _v2, glm::vec3 _v3, Material *mat):
-		Surface(mat), v1(_v1), v2(_v2), v3(_v3)
-	{}
+    Triangle(glm::vec3 _v1, glm::vec3 _v2, glm::vec3 _v3, Material *mat):
+        Surface(mat), v1(_v1), v2(_v2), v3(_v3)
+    {}
 
-	virtual bool hit(const Ray &ray, double t0, double t1, HitRec &hr);
+    virtual bool hit(const Ray &ray, double t0, double t1, HitRec &hr);
     virtual glm::vec3 getNormal(glm::vec3 hitpoint) const;
 };
 
