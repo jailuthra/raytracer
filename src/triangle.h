c263ce01ca8528c802bf30b20ef637ad5571eef0
abrolmrinal <mrinal15062@iiitd.ac.in>
2017-11-03 11:38:55 +0530

    Modified triangle

--- src/triangle.h
+++ src/triangle.h
@@ -2,11 +2,12 @@
 #define __TRIANGLE_H
     
 #include "surface.h"
-#include <glm/vec3.hpp>
+#include <glm/glm.hpp>
 
 class Triangle : public Surface {
 public: 
     glm::vec3 v1, v2, v3; 
+    glm::vec3 normal = glm::normalize(glm::cross(v2 - v1, v1 - v3));
 
     Triangle(glm::vec3 _v1, glm::vec3 _v2, glm::vec3 _v3, Material *mat):
         Surface(mat), v1(_v1), v2(_v2), v3(_v3)
